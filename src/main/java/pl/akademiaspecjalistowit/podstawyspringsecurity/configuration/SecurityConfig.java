a536bf95cd39e583aca94a80699a60d72c2f5844
przemyslawgesieniecasit <przemyslaw.gesieniec@gmail.com>
2024-10-04 17:22:52 +0200

    init commit

---
+++ src/main/java/pl/akademiaspecjalistowit/podstawyspringsecurity/configuration/SecurityConfig.java
@@ -0,0 +1,8 @@
+package pl.akademiaspecjalistowit.podstawyspringsecurity.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConfig {
+
+}
